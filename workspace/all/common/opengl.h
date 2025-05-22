740e78bd0eab95e0197459db103cd2ca800ed2b6
frysee <frysee@googlemail.com>
2025-05-22 10:24:28 +0200

    Fix gl linkage

--- workspace/all/common/opengl.h
+++ workspace/all/common/opengl.h
@@ -1,16 +1,22 @@
 #ifndef GL_HEADERS_H
 #define GL_HEADERS_H
 
-#ifdef USE_GL
-#    //pragma message("Compiling with OpenGL")
-#    include <OpenGL/gl3.h>
-#    include <OpenGL/gl3ext.h>
-#    define WHICH_GL "OpenGL"
-#else
-#    //pragma message("Compiling with OpenGL ES")
-#    define GL_GLEXT_PROTOTYPES 1
-#    include <GLES3/gl3.h>
-#    define WHICH_GL "GLESv2"
-#endif
+//#ifdef USE_GL
+//#   //pragma message("Compiling with OpenGL")
+//#   if defined(__APPLE__)
+//#       include <OpenGL/gl3.h>
+//#       include <OpenGL/gl3ext.h>
+//#   else
+//#       include <GL3/gl3.h>
+//#       include <GL3/gl3ext.h>
+//#   endif
+//#   define WHICH_GL "OpenGL"
+//#elif defined (USE_GLES)
+//#   //pragma message("Compiling with OpenGL ES 3")
+//#   include <GLES3/gl3.h>
+//#   define __gl2_h_
+//#   include <GLES2/gl2ext.h>
+//#   define WHICH_GL "GLESv2"
+//#endif
 
 #endif // GL_HEADERS_H
