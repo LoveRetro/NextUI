6f498dfb93b6d28fa18e544448fdeb09d3a8edc1
Robin Morgan <60886158+ro8inmorgan@users.noreply.github.com>
2025-08-07 17:33:58 +0200

    WiFi fix patch 2 (#464)
    * reverted the offloading back to OG config and also cpu governer

--- skeleton/SYSTEM/tg5040/etc/wifi/wifi_init.sh
+++ skeleton/SYSTEM/tg5040/etc/wifi/wifi_init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 start() {
-	echo "disable_scan_offload=0" > /etc/wifi/wpa_supplicant_overlay.conf
+	echo "disable_scan_offload=1" > /etc/wifi/wpa_supplicant_overlay.conf
 	rfkill.elf unblock wifi
 
 	/etc/init.d/wpa_supplicant start
