593ed8c00d6378ffab080db0f55f151bb2e4b507
frysee <frysee@googlemail.com>
2025-12-17 21:08:42 +0100

    Fix Remove Loading.pak

--- skeleton/EXTRAS/Tools/tg5050/Remove Loading.pak/launch.sh
+++ skeleton/EXTRAS/Tools/tg5050/Remove Loading.pak/launch.sh
@@ -3,7 +3,7 @@
 DIR="$(dirname "$0")"
 cd "$DIR"
 
-sed -i '/^\/usr\/sbin\/pic2fb \/etc\/splash.png/d' /etc/init.d/runtrimui
+sed -i '/^\/usr\/trimui\/bin\/sdl2display \/usr\/trimui\/bin\/splash.png \&/d' /mnt/SDCARD/.tmp_update/tg5050.sh
 show.elf "$DIR/$DEVICE/done.png" 2
 
 mv "$DIR" "$DIR.disabled"
