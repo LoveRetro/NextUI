2342dd76721e52b8c491f3cfa360c12cce3cd335
frysee <frysee@googlemail.com>
2025-12-20 01:49:39 +0100

    Add generic bluetooth implementation (no trimui libs this time)

--- skeleton/SYSTEM/tg5050/etc/bluetooth/bt_init.sh
+++ skeleton/SYSTEM/tg5050/etc/bluetooth/bt_init.sh
@@ -1,39 +1,26 @@
 #!/bin/sh
-# Taken from allwinner/btmanager/config/xradio_bt_init.sh for NextUI
-bt_hciattach="hciattach"
+# Bluetooth initialization script for NextUI
 
-start_hci_attach()
-{
-	h=`ps | grep "$bt_hciattach" | grep -v grep`
-	[ -n "$h" ] && {
-		killall "$bt_hciattach"
-	}
+DEVICE_NAME="Trimui Smart Pro S (NextUI)"
 
-	#xradio init
-	"$bt_hciattach" -n ttyS1 xradio >/dev/null 2>&1 &
-
-	wait_hci0_count=0
-	while true
-	do
-		[ -d /sys/class/bluetooth/hci0 ] && break
-		usleep 100000
-		let wait_hci0_count++
-		[ $wait_hci0_count -eq 70 ] && {
-			echo "bring up hci0 failed"
-			exit 1
-		}
-	done
-}
-
-start() {
-	rfkill.elf unblock bluetooth
-
-	if [ -d "/sys/class/bluetooth/hci0" ];then
-		echo "Bluetooth init has been completed!!"
-	else
-		start_hci_attach
+start_bt() {
+	# Load BT driver module if not loaded
+	if ! lsmod | grep -q aic8800_btlpm; then
+		modprobe aic8800_btlpm.ko 2>/dev/null
+		sleep 0.5
 	fi
 
+	# Start bluetooth service if not running
+	/etc/bluetooth/bt_init.sh start 2>/dev/null
+	#sleep 0.5
+
+	# Start hciattach if not running
+    hpid=`pgrep hciattach`
+    if [ "$hpid" == "" ] ; then
+        hciattach -n ttyAS1 aic &
+    fi        
+
+	# Start bluetooth daemon if not running
     d=`ps | grep bluetoothd | grep -v grep`
 	[ -z "$d" ] && {
 		/etc/bluetooth/bluetoothd start
@@ -46,71 +33,43 @@ start() {
 		bluealsa -p a2dp-source &
 		sleep 1
     }
+	
+	# Power on adapter
+	bluetoothctl power on 2>/dev/null
+	
+	# Set discoverable and pairable
+	bluetoothctl discoverable on 2>/dev/null
+	bluetoothctl pairable on 2>/dev/null
+	
+	# Set default agent for automatic pairing (no input/output)
+	bluetoothctl agent NoInputNoOutput 2>/dev/null
+	bluetoothctl default-agent 2>/dev/null
+	
+	# Set adapter name
+	bluetoothctl system-alias "$DEVICE_NAME" 2>/dev/null
 }
 
-ble_start() {
-	rfkill.elf unblock bluetooth
-
-	if [ -d "/sys/class/bluetooth/hci0" ];then
-		echo "Bluetooth init has been completed!!"
-	else
-		start_hci_attach
-	fi
-
-	hci_is_up=`hciconfig hci0 | grep RUNNING`
-	[ -z "$hci_is_up" ] && {
-		hciconfig hci0 up
-	}
-
-	MAC_STR=`hciconfig | grep "BD Address" | awk '{print $3}'`
-	LE_MAC=${MAC_STR/2/C}
-	OLD_LE_MAC_T=`cat /sys/kernel/debug/bluetooth/hci0/random_address`
-	OLD_LE_MAC=$(echo $OLD_LE_MAC_T | tr [a-z] [A-Z])
-	if [ -n "$LE_MAC" ];then
-		if [ "$LE_MAC" != "$OLD_LE_MAC" ];then
-			hciconfig hci0 lerandaddr $LE_MAC
-		else
-			echo "the ble random_address has been set."
-		fi
-	fi
-}
-
-stop() {
-	a=`ps | grep bluealsa | grep -v grep`
-	[ -n "$a" ] && {
-		killall bluealsa
-		sleep 1
-	}
-
-	d=`ps | grep bluetoothd | grep -v grep`
-	[ -n "$d" ] && {
-		killall bluetoothd
-		sleep 1
-	}
-
-	h=`ps | grep "$bt_hciattach" | grep -v grep`
-	[ -n "$h" ] && {
-		killall "$bt_hciattach"
-		sleep 1
-	}
-
-	rfkill.elf block bluetooth
-	#echo 0 > /sys/class/rfkill/rfkill0/state;
-	sleep 1
-	echo "stop bluetoothd and hciattach"
+stop_bt() {
+	# Stop bluetooth service
+	/etc/bluetooth/bt_init.sh stop 2>/dev/null
 }
 
 case "$1" in
-  start|"")
-        start
-        ;;
-  stop)
-        stop
-        ;;
-  ble_start)
-	    ble_start
+	start)
+		start_bt
 		;;
-  *)
-        echo "Usage: $0 {start|stop}"
-        exit 1
+	stop)
+		stop_bt
+		;;
+	restart)
+		stop_bt
+		sleep 0.5
+		start_bt
+		;;
+	*)
+		echo "Usage: $0 {start|stop|restart}"
+		exit 1
+		;;
 esac
+
+exit 0
