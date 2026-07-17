ff202893e2d1a827450f15d39222d0be477f471b
frysee <frysee@googlemail.com>
2026-07-17 23:58:49 +0200

    feat: align Brick/Pro rumble strength a bit more

--- workspace/tg5040/platform/platform.c
+++ workspace/tg5040/platform/platform.c
@@ -312,7 +312,8 @@ void PLAT_setCPUSpeed(int speed) {
 
 #define MAX_STRENGTH 0xFFFF
 #define MIN_VOLTAGE 500000
-#define MAX_VOLTAGE 3300000
+// Brick Pro has a 3.3V motor driver, but its getting very annoying on higher rumble strengths, so we limit it to 2.5V for now.
+#define MAX_VOLTAGE (is_brickpro ? 2500000 : 3300000)
 #define RUMBLE_PATH "/sys/class/gpio/gpio227/value"
 #define RUMBLE_VOLTAGE_PATH "/sys/class/motor/voltage"
 
